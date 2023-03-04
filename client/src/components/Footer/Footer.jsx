e9aba9ecce5f77e71e6442a4e4d4210878bdf45c
hokageCV <chaitanyavaru@gmail.com>
2023-03-04 22:32:16 +0530

    auto update footer year

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
-import { Box } from '@mui/system';
-import React from 'react';
-import styles from './style';
+import { Box } from "@mui/system";
+import React from "react";
+import styles from "./style";
 
-export default function Footer(){
-    return(
-        <Box sx={styles.footer}>
-            <footer>Copyright &copy; 2023. ShareNaam.pvt.Ltd</footer>
-        </Box>
-    )
+export default function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <Box sx={styles.footer}>
+      <footer>Copyright &copy; {year}. ShareNaam.pvt.Ltd</footer>
+    </Box>
+  );
 }
